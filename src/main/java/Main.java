96429c30f5724f9bf4db842ede0c9689ecfbfb63
maksprz987 <maszywa2810@gmail.com>
2025-03-19 13:46:04 +0000

    koniec

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,13 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-import java.util.Random;
+
 
 public class Main {
-
-  
-  Random random = new Random();
-  int losowaLiczba = random.nextInt(101); // generates number from 0 to 100
-
-  System.out.println("Wylosowana liczba: " + losowaLiczba);
+  public static void main(String[] args) {
+    System.out.println((int)(Math.random() * 101));
   }
-  }
+}
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
